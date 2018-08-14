e008e4a8e2c7284b0c381ff49d3566262e207cb3
chloeharrison18 <chloe.harrison@my.jcu.edu.au>
2018-08-14 12:34:28 +1000

    Checks password

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,11 @@
 """ Chloe Harrison """
+
+MIN_LENGTH = 5
+
+password = input("Enter password: ")
+
+while len(password) < MIN_LENGTH:
+    print("Needs 5 characters")
+    password = input("Enter password: ")
+
+print("Valid password")
